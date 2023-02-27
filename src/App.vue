9cf1e6fb68ac89e40ed957037bf78820bb38e794
ntdat19122002 <ntdat19122002@gmail.com>
2023-02-27 21:19:03 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- <header>
+
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+
+  <RouterView />
+</template>
+
+<style>
+#app{
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/views/Create.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="create">
+    <form>
+        <label for="">Title:</label>
+        <input type="text" required>
+        <label for="">Content:</label>
+        <textarea v-model="body" required></textarea>
+        <label>Tags (hit enter to add a tag)</label>
+        <input v-model="tag" type="text" name="" id="">
+        <button>Add Post</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
