68cd6ad39d6608b17f3118d6db1878d4475887e9
ntdat <ntdat19122002@gmail.com>
2023-02-27 23:41:12 +0700

    post

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Details from '../views/Details.vue'
+import Create from '../views/Create.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'Details',
       component: Details,
       props:true
+    },
+    {
+      path: '/create',
+      name: 'Create',
+      component: Create,
     },
   ]
 })
